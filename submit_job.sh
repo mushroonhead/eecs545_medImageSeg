878867fff983b8aad560bdc6454646ae2300c57f
Thomas Joseph Gregart <tgregart@gl-login3.arc-ts.umich.edu>
2023-03-23 11:38:11 -0400

    added great lakes functionality

--- submit_job.sh
+++ submit_job.sh
@@ -4,7 +4,7 @@
 #SBATCH --account=eecs545w23_class
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=5g
-#SBATCH --time=02:00:00
+#SBATCH --time=08:00:00
 #SBATCH --partition=gpu
 #SBATCH --gpus=2
 #SBATCH --mail-type=NONE
